aff28f3f0697c1d4cc5f1151c6866344bbc4a513
souzaramon <ramon.souza@objective.com.br>
2022-11-18 00:01:07 -0300

    chore: set baseurl in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/template-vite-react',
   server: {
     port: 8080,
   },
